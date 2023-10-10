2ae35e9317cd5ed1c970bbbefb98640abf90c60c
Rasim <rasimseferli98@gmail.com>
2023-10-10 17:54:57 +0400

    VehicleController changed

--- CrudApp/src/main/java/com/project/crud/controllers/VehicleController.java
+++ CrudApp/src/main/java/com/project/crud/controllers/VehicleController.java
@@ -1,4 +1,18 @@
 package com.project.crud.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class VehicleController {
+    private final VehicleService vehicleService;
+
+    public ResponseEntity<List<VehicleDto>> allVehicles() {
+        return ResponseEntity.ok(vehicleService.allVehicles());
+    }
+
 }
